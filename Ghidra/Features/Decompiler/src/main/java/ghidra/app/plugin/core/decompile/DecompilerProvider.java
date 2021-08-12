f8e5caf8de0012bf7489fa9a15ffc9347dfe37e3
ioo0s <iosmosis@163.com>
2021-08-12 14:15:50 +0800

    add convert to float/double plugin for decompiler

--- Ghidra/Features/Decompiler/src/main/java/ghidra/app/plugin/core/decompile/DecompilerProvider.java
+++ Ghidra/Features/Decompiler/src/main/java/ghidra/app/plugin/core/decompile/DecompilerProvider.java
@@ -1,4 +1,4 @@
-/* ###
+ /* ###
  * IP: GHIDRA
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -890,6 +890,12 @@ public class DecompilerProvider extends NavigatableComponentProviderAdapter
 		ConvertCharAction convertCharAction = new ConvertCharAction(plugin);
 		setGroupInfo(convertCharAction, convertGroup, subGroupPosition++);
 
+		ConvertFloatAction convertFloatAction = new ConvertFloatAction(plugin);
+		setGroupInfo(convertFloatAction, convertGroup, subGroupPosition++);
+
+		ConvertDoubleAction convertDoubleAction = new ConvertDoubleAction(plugin);
+		setGroupInfo(convertDoubleAction, convertGroup, subGroupPosition++);
+
 		//
 		// Comments
 		//
@@ -967,6 +973,8 @@ public class DecompilerProvider extends NavigatableComponentProviderAdapter
 		addLocalAction(convertHexAction);
 		addLocalAction(convertOctAction);
 		addLocalAction(convertCharAction);
+		addLocalAction(convertFloatAction);
+		addLocalAction(convertDoubleAction);
 		addLocalAction(setEquateAction);
 		addLocalAction(removeEquateAction);
 		addLocalAction(retypeLocalAction);
